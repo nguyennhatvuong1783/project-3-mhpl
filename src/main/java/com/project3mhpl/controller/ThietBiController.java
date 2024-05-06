eb4a5f339e8cf0af2d70d6fb16d278a2e91d1fab
tranqui268 <trannhatqui712@gmail.com>
2024-05-06 10:20:08 +0700

    feat: thietbi

--- src/main/java/com/project3mhpl/controller/ThietBiController.java
+++ src/main/java/com/project3mhpl/controller/ThietBiController.java
@@ -4,7 +4,6 @@
  */
 package com.project3mhpl.controller;
 
-import com.project3mhpl.entity.FormData;
 import com.project3mhpl.entity.ThietBi;
 import com.project3mhpl.entity.ThongTinSD;
 import com.project3mhpl.repository.ThietBiRepository;
@@ -18,6 +17,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -39,19 +40,23 @@ public class ThietBiController {
             products.add(t);
         }
 
-        m.addAttribute("formData",new FormData());
         m.addAttribute("data", products);
         return "thiet_bi_all";
     }
     
-    @PostMapping("/submit-data")
-   public String submitData(@ModelAttribute("formData") FormData formData) {
+   @RequestMapping(value = "/dat-muon-thiet-bi", method = RequestMethod.POST)
+   public String submitData(@RequestParam("date") String date, Model m) {
     // Xử lý dữ liệu và trả về view hoặc thực hiện các hành động khác
-    String submittedData = formData.getData();
     // ...
-    return "result-page";
+     return "redirect:/";
   }
 
+    @RequestMapping(value = "/muonthietbi", method = RequestMethod.POST)
+   public String submitData2(@RequestParam("date") String date, Model m) {
+    // Xử lý dữ liệu và trả về view hoặc thực hiện các hành động khác
+    // ...
+     return "redirect:/";
+  }
     
    
     

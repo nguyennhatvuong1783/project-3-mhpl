09731982eb3cc278c613d104a9740d462bee518e
XiuQuy <thieuhuy.90603@gmail.com>
2024-05-13 13:32:06 +0700

    hot fix

--- src/main/java/com/project3mhpl/service/XulyService.java
+++ src/main/java/com/project3mhpl/service/XulyService.java
@@ -2,8 +2,12 @@ package com.project3mhpl.service;
 
 import com.project3mhpl.entity.XuLy;
 import com.project3mhpl.repository.XuLyRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.WebUtils;
 
 /**
  *
@@ -30,4 +34,10 @@ public class XulyService {
     public Boolean saveXl(XuLy xl) {
         return xuLyRepository.save(xl) != null;
     }
+    @SuppressWarnings("null")
+	public List<XuLy> getTTXLByIdTV(HttpServletRequest request) {
+		Cookie c = WebUtils.getCookie(request, "auth");
+		List<XuLy> authUser = xuLyRepository.findByThanhVienXL_MaTV(Integer.parseInt(c.getValue()));
+		return authUser;
+	}
 }
